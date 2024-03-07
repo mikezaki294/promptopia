ffe64b992ddd4dd52861938be046511cfd3d5210
michael zaki <mikezaki294@gmail.com>
2024-03-07 14:20:44 -0800

    reverted prev comment

--- app/edit-prompt/page.jsx
+++ app/edit-prompt/page.jsx
@@ -1,69 +1,69 @@
-// "use client";
+"use client";
 
-// import { useState, useEffect } from "react";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { Suspense } from "react";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-// import Form from "@components/Form";
+import Form from "@components/Form";
 
-// const EditPrompt = () => {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   const promptId = searchParams.get("id");
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
 
-//   const [submitting, setSubmitting] = useState(false);
-//   const [post, setPost] = useState({
-//     prompt: "",
-//     tag: "",
-//   });
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
-//   useEffect(() => {
-//     const getPromptDetails = async () => {
-//       const response = await fetch(`/api/prompt/${promptId}`);
-//       const data = await response.json();
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-//       setPost({
-//         prompt: data?.prompt,
-//         tag: data?.tag,
-//       });
-//     };
+      setPost({
+        prompt: data?.prompt,
+        tag: data?.tag,
+      });
+    };
 
-//     if (promptId) getPromptDetails();
-//   }, [promptId]);
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
-//   const updatePrompt = async (e) => {
-//     e.preventDefault();
-//     setSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-//     if (!promptId) return alert("");
-//     try {
-//       const response = await fetch(`/api/prompt/${promptId}`, {
-//         method: "PATCH",
-//         body: JSON.stringify({
-//           prompt: post.prompt,
-//           tag: post.tag,
-//         }),
-//       });
+    if (!promptId) return alert("");
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
 
-//       if (response.ok) {
-//         router.push("/");
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setSubmitting(false);
-//     }
-//   };
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
-//   return (
-//     <Form
-//       type="Edit"
-//       post={post}
-//       setPost={setPost}
-//       submitting={submitting}
-//       handleSubmit={updatePrompt}
-//     />
-//   );
-// };
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
 
-// export default EditPrompt;
+export default EditPrompt;
